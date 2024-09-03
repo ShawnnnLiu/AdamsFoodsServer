8bb57c9f381e178a0e1b40eae8a47bd78dd57a86
Anthony Suh <anthods1@uci.edu>
2024-09-03 16:07:16 -0700

    Fixed up inventoryFind

--- index.js
+++ index.js
@@ -2,14 +2,72 @@ const express = require ("express")
 const mongoose = require ("mongoose")
 const cors = require ("cors")
 const UserModel = require("./models/User")
+const FreezerModel = require("./models/Freezer")
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb+srv://shawn:Liu123456@atlascluster.frneldu.mongodb.net/AdamsFoods")
+//mongoose.connect("mongodb+srv://tester:testing12345@atlascluster.frneldu.mongodb.net/AdamsFoods")
+
+mongoose.connect("mongodb+srv://anthony:1Anthony@atlascluster.frneldu.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
 .then(() => console.log('Connected to MongoDB Atlas'))
 .catch(err => console.error('Connection error', err));
+
+
+app.post('/updateSubmit', (req, res) => {
+    const inputs = req.body.inputs;
+  
+    // Perform whatever processing you need with the inputs
+    console.log('Received inputs:', inputs);
+
+    res.json(inputs);
+  });
+
+  // inventoryFind
+  app.post('/inventoryFind', (req, res) => {
+    const { location, lot, vendor, brand, species, description, grade, quantity, weight, packdate, temp, est } = req.body.inputs || {};
+
+    const query = {};
+
+    if (location) query.location = location;
+    if (lot) query.lot = lot;
+    if (vendor) query.vendor = vendor;
+    if (brand) query.brand = brand;
+    if (species) query.species = species;
+    if (description) query.description = description;
+    if (grade) query.grade = grade;
+    if (quantity) query.quantity = quantity;
+    if (weight) query.weight = weight;
+    if (packdate) query.packdate = packdate;
+    if (temp) query.temp = temp;
+    if (est) query.est = est;
+
+    console.log('+++++++++++++++++++++++++++++++++++');
+    console.log('Query:', query);
+    console.log('Searching...');
+
+    FreezerModel.find(query)
+        .then(items => {
+            if (items.length > 0) {
+                console.log('----------------------');
+                console.log('Found items:', items);
+                res.json(items);
+            } else {
+                // Send a 404 status if no items are found
+                console.log('No items found');
+                res.status(404).json({ message: 'No items found' });
+            }
+        })
+        .catch(err => {
+            // Log the error and send a 500 status if something goes wrong
+            console.error('Error during database operation:', err);
+            res.status(500).json({ error: 'An error occurred while retrieving the items.' });
+        });
+});
+
+  
+
 
 app.post("/login", (req, res) => {
     const { email, password } = req.body;
@@ -50,5 +108,3 @@ app.post("/signup", (req, res) => {
   app.listen(3001, () => {
     console.log("server is running")
   })
-
-
---
+++ models/Freezer.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+// Define the schema for the user
+const FreezerSchema = new mongoose.Schema({
+    location: String,
+    lot: String,
+    vendor: String,
+    brand: String,
+    species: String,
+    description: String,
+    grade: String,
+    quantity: String,
+    weight: String,
+    packdate: String,
+    temp: String,
+    est: String
+});
+
+// Connect the schema to the `UserDatabase` collection
+const FreezerModel = mongoose.model("freezerinventories", FreezerSchema);
+
+module.exports = FreezerModel;
